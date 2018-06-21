ead0314cec4660bfa8a2468471d6d3b206c3c5f3
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-21 15:44:54 +0000

    Cart fix and some comments

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  # resources :login 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
