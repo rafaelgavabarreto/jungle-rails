aaebac78cb884796455dc223419fd7f7d6474f8c
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-21 13:06:01 +0000

    Fix the categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :category, only: [:index, :create, :new]
+    resources :categories, only: [:index, :create, :new]
   end
 
   # Thouse routes are to users. Create and sign in.
